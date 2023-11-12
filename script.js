7255bae5bf71e15359c2c4f7d5b3b0170c81160d
Kekyra228 <katyiv02@gmail.com>
2023-11-12 16:49:23 +0400

    доделала верстку сайта и добавила js

---
+++ script.js
@@ -0,0 +1,56 @@
+let age =10;
+//alert (10);
+let number1=2
+
+let year=2007;
+//alert (year)
+
+let nameCreater="Джеймс Гослинг";
+//alert ("Джеймс Гослинг");
+
+let result1 = age + number1;
+console.log (result1);
+
+let result2 = age - number1;
+console.log (result2);
+
+let result3 = age * number1;
+console.log (result3);
+
+let result4 = age / number1;
+console.log (result4);
+
+let result5 = number1 ** 5;
+console.log (result5); //алерт
+
+let a = 9;
+let b = 2;
+let result6= 9%2;
+//alert (`задание 6 ${result6}`); 
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+console.log  (num); //алерт
+
+
+// let age1 = Number(prompt ("Сколько вам лет?"));
+let resultAge = age1;
+//alert(`Вам ${age1} лет`); 
+
+let user = {
+    name: 'kit',
+    ageUser:22,
+    isAdmin:true,
+    cityofresidence:"Samara",
+}
+
+// let info = prompt ('Какую информацию вы хотите узнать о пользователе?');
+//alert(user[info]); 
+
+let userName = prompt ("Ваше имя?");
+//alert(`Привет, ${userName}!`)
