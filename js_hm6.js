ac90ef5e3493932994c93f02fdc631d5e3e8088d
Kekyra228 <katyiv02@gmail.com>
2023-11-26 15:56:55 +0400

    5 дз функции

---
+++ js_hm6.js
@@ -0,0 +1,96 @@
+// 1 звдвние
+// const arr = [1, 5, 4, 10, 0, 3];
+// for (let  = items0; items< arr.length; items++) {
+        console.log(arr[i]);
+//     if (arr[items]===10) break;
+//     console.log(arr[items])
+// }
+
+
+
+// 2 задание 
+// const array = [1, 5, 4, 10, 0, 3];
+// console.log(array.indexOf(4));
+
+
+
+// задание 3 почему не работает через const
+// let mass = [1, 3, 5, 10, 20];
+// mass = mass.join(' ');
+// console.log(mass);
+
+
+
+// // 4 задание
+// const arr = [
+//     [1, 1, 1],
+//     [1, 1, 1],
+//     [1, 1, 1]
+// ];
+// for (let arrEl of arr) {
+//     for (let itemsEl of  arrEl) {
+//         console.log (itemsEl)
+//     }
+// }
+
+
+
+// // задание 5
+// let massiv = [1, 1, 1];
+// massiv.push(2, 2, 2);
+// console.log(massiv)
+
+
+// 6 задание
+// const arr = [9, 8, 7, 'a', 6, 5];
+// arr.sort();
+// console.log(arr);
+// arr.pop('a');
+// console.log(arr)
+
+
+// задание 7 НЕ ПОНЯЛА  
+const mass = [9, 8, 7, 6, 5];
+// let guessNum =Number(prompt("Угадай число"));
+// let answer = guessNum;
+// for (let item of mass) {
+
+//     if (answer===mass) {
+//         alert("угадал")
+//     } else {
+//         alert("Не угадал")
+//     }
+// }
+
+
+// задание 8  ПОД ВОПРОСОМ надо ли делать join НАДО
+const str = 'abcdef';
+// const arr = [...str];
+// console.log(arr)
+// console.log(arr.reverse(arr))
+
+
+// задание 9
+// const arr = [[1, 2, 3,],[4, 5, 6]];
+// const mainarray = arr.flat();
+// console.log(mainarray)
+
+// 10 задание
+// const randomass = [2, 4, 5, 7, 8, 3];
+// const result = randomass.reduce((a, b)=> a + b);
+// console.log(result)
+
+
+// 11 задание  РЕТЕРН
+// const arr = [1, 4, 6, 8];
+// const result = arr.map(el => (el**2));
+// console.log(result);
+
+// 12 задание ???
+let getLengthWords = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+let result = getLengthWords.map((item) => {
+return item.length 
+})
+console.log(result)
+
+
