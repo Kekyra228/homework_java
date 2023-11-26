a5b65f4f69def5ca8395a369b28ac1f79d605018
Kekyra228 <katyiv02@gmail.com>
2023-11-26 16:52:45 +0400

    6 дз массивы

---
+++ js_hm6.js
@@ -0,0 +1,104 @@
+// 1 звдвние
+// const arr = [1, 5, 4, 10, 0, 3];
+// for (let  = items0; items< arr.length; items++) {
+    // console.log(arr[i]);
+    //     if (arr[items]===10) break;
+    //     console.log(arr[items])
+    // }
+    
+    
+    
+    // 2 задание 
+    // const array = [1, 5, 4, 10, 0, 3];
+    // console.log(array.indexOf(4));
+    
+    
+    
+    // задание 3 
+    // let mass = [1, 3, 5, 10, 20];
+    // let arr = mass.join(' ');
+    // console.log(arr);
+    
+    
+    
+    // 4 задание
+//     let arr = []
+// for (let index = 0; index < 3; index++) {
+//     const elementArr = [];
+//     for (let index2 = 0; index2 < 3; index2++) {
+//         elementArr.push(1);
+//     }
+//     arr.push(elementArr)
+// }
+// console.log(arr)
+    
+    
+    
+    // // задание 5
+    // let massiv = [1, 1, 1];
+    // massiv.push(2, 2, 2);
+    // console.log(massiv)
+    
+    
+    // 6 задание
+    // const arr = [9, 8, 7, 'a', 6, 5];
+    // arr.sort();
+    // console.log(arr);
+    // arr.pop('a');
+    // console.log(arr)
+    
+    
+    // задание 7  
+    // const mass = [9, 8, 7, 6, 5];
+    // let guessNum =Number(prompt("Угадай число"));
+    //     if (mass.includes(guessNum)) {
+    //          alert("угадал")
+    //     } else {
+    //         alert("Не угадал")
+    //      }
+    
+    
+    
+    // задание 8  
+    // const str = 'abcdef';
+    // const mass = [...str];
+    // mass.reverse();
+    // mass.join("")
+    // console.log(mass)
+    
+    
+    // задание 9
+    // const arr = [[1, 2, 3,],[4, 5, 6]];
+    // const mainarray = arr.flat();
+    // console.log(mainarray)
+    
+    // 10 задание
+    // const randomass = [2, 4, 5, 7, 8, 3];
+    // for (let index = 0; index < array.length-1; index++) {
+    //     console.log(randomass[index] + randommass[index + 1]);
+        
+    // }
+    
+    
+    // 11 задание  
+    // const arr = [1, 4, 6, 8];
+    // const result = arr.map(el => (el**2));
+    // console.log(result);
+    
+    // 12 задание ???
+    // function getWords(words) {
+    //     return words.map((getlenhWords) => getlenhWords.lenght)
+    // }
+    // console.log(getWords['слово', '', 'слог', 'длинное предложение', 'буква'])
+
+    // 13 
+    function filterPositive(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]< 0) {
+            result.push(array[i])
+        }
+    }
+    return result
+ }
+ filterPositive([-1, 0, 5, -10, 56]);
